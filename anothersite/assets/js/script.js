f99b1d1ff4bcc31634e6f7a407631eb9bd731a9c
asraful <ahsraful.freelancer@gmail.com>
2022-07-02 23:52:58 +0600

    slideshow

--- anothersite/assets/js/script.js
+++ anothersite/assets/js/script.js
@@ -1,8 +1,25 @@
-const point = document.querySelector('#header .top-btn .btn');
-const cat = document.querySelector('body');
+// const point = document.querySelector('#header .top-btn .btn');
+// const cat = document.querySelector('body');
 
-point.addEventListener('click',()=>{
-    point.classList.toggle('active');
-    cat.classList.toggle('active');
+// point.addEventListener('click',()=>{
+//     point.classList.toggle('active');
+//     cat.classList.toggle('active');
     
-});
+// });
+
+const slide = document.querySelectorAll('.slide-contant');
+
+let countEle = 1;
+
+setInterval(() => {
+    countEle++;
+    const currentEle =document.querySelector(".current");
+    currentEle.classList.remove("current");
+    if(countEle>slide.length){
+        slide[0].classList.add("current");
+        countEle = 1;
+    }else{
+        currentEle.nextElementSibling.classList.add("current");
+    }
+    
+}, 2000);
